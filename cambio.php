f0436be4aeeb76e9476c6bf10c2d23d7b3057db8
DarkMaster38 <esteban_dm38@hotmail.com>
2017-05-23 15:01:44 -0600

    Commit inicial

---
+++ cambio.php
@@ -0,0 +1,21 @@
+<?php
+include("utilerias.php");
+
+$conexion=conecta();//servidor y bd
+$u=GetSQLValueString($_GET["txtUsuario"],"text");
+$consulta=sprintf("select * from usuarios where usuario=%s",$u);
+$resultado=mysql_query($consulta);
+
+if(mysql_num_rows($resultado)>0){
+	if($registro=mysql_fetch_array($resultado)){
+			print("<form action='guardacambio.php' method='post'>");
+			print("<input type='text' placeholder='usuario' name='txtUsuario' value=".$registro["usuario"]."><br>");
+			print("<input type='text' placeholder='nombre' name='txtNombre' value='".$registro["nombre"]."''><br>");
+			print("<input type='password' placeholder='clave' name='txtClave'value=".$registro["clave"]."><br>");
+			print("<input type='text' placeholder='depto' name='txtDepto' value=".$registro["departamento"]."><br>");
+			print("<input type='text' placeholder='vigencia' name='txtVigencia' value=".$registro["vigencia"]."><p>");
+			print("<input type='submit' value='Guardar'>");
+			print("</form>");
+	}
+}
+?>
